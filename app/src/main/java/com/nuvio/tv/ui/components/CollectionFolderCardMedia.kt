76c5658bb88f786e40be8d1046f1db988b409dac
LostInTyping <Benarmour72@gmail.com>
2026-04-14 22:35:16 -0400

    feat(collections): add animated focus request helper

--- app/src/main/java/com/nuvio/tv/ui/components/CollectionFolderCardMedia.kt
+++ app/src/main/java/com/nuvio/tv/ui/components/CollectionFolderCardMedia.kt
@@ -1,6 +1,11 @@
 package com.nuvio.tv.ui.components
 
+import android.content.Context
+import coil.request.ImageRequest
+import coil.request.repeatCount
 import com.nuvio.tv.domain.model.CollectionFolder
+
+const val ANIMATED_FOCUS_DEBOUNCE_MS = 200L
 
 fun collectionFolderCardImageUrl(
     folder: CollectionFolder,
@@ -21,6 +26,29 @@ fun collectionFolderBackdropUrl(folder: CollectionFolder): String? {
         return firstNonBlank(folder.backdropImageUrl)
     }
     return firstNonBlank(folder.backdropAnimatedUrl, folder.backdropImageUrl)
+}
+
+fun buildAnimatedFocusRequest(
+    context: Context,
+    url: String,
+    playOnce: Boolean,
+    focusSessionKey: Int,
+    width: Int,
+    height: Int
+): ImageRequest {
+    return ImageRequest.Builder(context)
+        .data(url)
+        .crossfade(false)
+        .apply {
+            if (playOnce) {
+                repeatCount(0)
+                memoryCacheKey("${url}_${width}x${height}_session$focusSessionKey")
+            } else {
+                memoryCacheKey("${url}_${width}x${height}")
+            }
+        }
+        .size(width = width, height = height)
+        .build()
 }
 
 private fun firstNonBlank(vararg candidates: String?): String? {
